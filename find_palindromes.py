ec667758677fd4ad059ba5d491293f163a510f3c
AkaiNoCat <46026384+AkaiNoCat@users.noreply.github.com>
2019-06-06 10:08:56 +0800

    Update find_palindromes.py
    增加了中心扩展法，从leetcode学习到的思路。

--- find_palindromes.py
+++ find_palindromes.py
@@ -24,6 +24,27 @@ def aline_find_Palindrome(string):
 # 长度n的字符串在string中遍历,应该需要0到len(string)-n次,也就是range(len(string)-n+1)
 # 同时 n的长度应该大于2 小于len(string)+1次 所以就是 range(2,len(string)+1)     
 
+
+# 中心扩展法
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        if len(s) < 2 or s == s[::-1]:
+            return s
+        n = len(s)
+        self.res = ""
+        def check(i,j):
+            while i >= 0 and j < n and s[i] == s[j]:
+                i -= 1
+                j += 1
+            if len(self.res) < j - i -1 :
+                self.res = s[i+1:j]
+        for i in range(n):
+            j=i
+            check(i,j)
+            check(i,j+1)
+        return self.res
+
+    
 if __name__ == "__main__":
     string = input('please input ling_string:')
     print(find_Palindrome(string))
